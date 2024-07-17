c8419472ccdf36de8965209739f83263da1ced88
balfiere <balfiere@proton.me>
2024-07-17 13:10:04 -0500

    add function that gets human's choice of rock, paper, or scissors

--- script.js
+++ script.js
@@ -8,10 +8,22 @@ function getComputerChoice() {
         return "rock";
     } else if (randomNumber < 0.66) { // Else if the number is between 0.33 and 0.66, return "paper"
         return "paper";
-    }
-    else { // Else, return "scissors"
+    } else { // Else, return "scissors"
         return "scissors";
     }
 }
 
 // Get the user's choice of rock, paper, or scissors
+function getHumanChoice() {
+    // Show a prompt to the user to select rock, paper, or scissors
+    let userChoice = prompt("Please type rock, paper, or scissors");
+    userChoice = userChoice.toLowerCase();
+
+    // Check to make sure user inputted rock, paper, or scissors. If not, prompt again
+    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        userChoice = prompt("Please type rock, paper, or scissors");
+        userChoice = userChoice.toLowerCase();
+    }
+
+    return userChoice;
+}

2068ec236549dc0369f3058c4fbd51d63827c4a7
balfiere <balfiere@proton.me>
2024-07-17 12:58:37 -0500

    create function that gets computer's choice of rock, paper, or scissors

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get the computer's choice of rock, paper, or scissors
+function getComputerChoice() {
+
+    // Get a random number between 0 and 1
+    let randomNumber = Math.random();
+    
+    if (randomNumber < 0.33) { // If the number is less than 0.33, return "rock"
+        return "rock";
+    } else if (randomNumber < 0.66) { // Else if the number is between 0.33 and 0.66, return "paper"
+        return "paper";
+    }
+    else { // Else, return "scissors"
+        return "scissors";
+    }
+}
+
+// Get the user's choice of rock, paper, or scissors
